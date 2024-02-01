23b5bf365cd71c8bff2216a146e299d2d8e422c4
Reaksmey RT <39754235+ReaksmeyThan@users.noreply.github.com>
2024-02-01 21:11:40 +0700

    spring bean
    spring bean

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/PocManGame.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/PocManGame.java
@@ -3,19 +3,19 @@ package com.in28minutes.learnspringframework.game;
 public class PocManGame implements GamingConsole {
 
 	public void up() {
-		System.out.println("up");
+		System.out.println("PocManGame_up");
 	}
 	
 	public void down() {
-		System.out.println("down");
+		System.out.println("PocManGame_down");
 	}
 	
 	public void left() {
-		System.out.println("left");
+		System.out.println("PocManGame_left");
 	}
 	
 	public void right() {
-		System.out.println("right");
+		System.out.println("PocManGame_right");
 	}
 	
 
--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/helloworld/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
-package com.in28minutes.learnspringframework;
+package com.in28minutes.learnspringframework.helloworld;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 //Persion2
 record Person(String name ,int age,Address address) {};
@@ -20,7 +22,6 @@ public class HelloWorldConfiguration {
 	public int  age() {
 		return 32;
 	}
-	
 	@Bean
 	public Person  person() {
 		return new Person("Mey Mey",23,new Address("PP", "Cambo"));
@@ -31,19 +32,35 @@ public class HelloWorldConfiguration {
 		return new Person(name(),age(),address());
 	}
 	
-	@Bean
+	@Bean	
 	public Person  person3Parameters(String name,int age,Address address3) {
 		return new Person(name,age,address3);
 	}
 	
+	@Bean
+	@Primary
+	// No qualifying bean of type 'com.in28minutes.leanspringframework.Adress'
+	//available: expected single matching bean but found 2: address2, address3
+	public Person  person4Parameters(String name,int age,Address address) {
+		// name , age , address2
+		return new Person(name,age,address);
+	}
 	
+	@Bean
 	
-	@Bean(name="address2")
+	public Person person5Qualifier(String name,int age,@Qualifier("address3Qualifier")Address address) {
+		// name , age , address2
+		return new Person(name,age,address);
+	}
+	
+	@Bean(name="address2")	
+	@Primary 
 	public Address  address() {
 		return new Address("Cambodia ","PhnomPenh");
 	}
 	
 	@Bean(name="address3")
+	@Qualifier("address3Qualifier")
 	public Address  address3() {
 		return new Address("BangKok ","Thailand");
 	}
